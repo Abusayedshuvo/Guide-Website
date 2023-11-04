5fc01b1ff9906c40e54b9e0e683841223f6a409a
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2023-11-05 00:22:58 +0600

    Error Page Design and back to home button add

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import Img404 from "../assets/404.json";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <p className="text-8xl">Error page</p>
+    <div className="container mx-auto text-center">
+      <Lottie
+        className="w-2/5 mx-auto mb-0"
+        animationData={Img404}
+        loop={true}
+      />
+      ;<p className="text-4xl font-bold mb-5">Oops!</p>
+      <p> We're sorry,</p>
+      <p> The page you were looking for doesn't exist anymore.</p>
+      <Link
+        t="/"
+        className="inline-block p-4 w-1/2 bg-primary text-white rounded-lg font-semibold mt-10"
+      >
+        Back to Home
+      </Link>
     </div>
   );
 };
